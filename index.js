52fdb86f8816b37c0069755f9f0a4de5ed4d936e
Naman Chandok <namanchandok1@gmail.com>
2021-08-21 23:58:00 +0530

    E

--- index.js
+++ index.js
@@ -14,10 +14,27 @@ function showPage() {
 
 jQuery(document).on("scroll", function () {
     if ($(document).scrollTop() > 120) {
-        $(".navbar").css("background-color","#090b0d");
-        $(".navbar a").css("color","#fff");
+        $(".navbar").css("background-color", "#090b0d");
+            $(".navbar a").css("color", "#fff");
     } else {
-        $(".navbar").css("background","none");
-        $(".navbar a").css("color","#000");
+        $(".navbar").css("background", "none");
+            $(".navbar a").css("color", "#000");
     }
-});   
+});
+
+// MUSIC THING
+
+var playing = false
+
+function play() {
+    if (playing == true) {
+        playing = false;
+        document.getElementById('play-btn').innerHTML = "<i class='fal fa-play'></i>";
+        document.getElementById('try-audio').pause();
+    } else if (playing == false) {
+        playing = true;
+        document.getElementById('play-btn').innerHTML = "<i class='fal fa-pause'></i>";
+        document.getElementById('try-audio').play();
+    }
+    console.log(playing)
+}
